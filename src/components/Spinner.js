b8788ebf1e5621acfa24148c1ff9919475488d3d
SAMUEL CHERIAN JOB <126221026+SamuelJob248@users.noreply.github.com>
2025-04-28 15:29:40 +0530

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,8 @@
-// src/components/Spinner.js
+
 import React from 'react';
 
 function Spinner() {
-  // We apply styling via CSS class 'loader' defined in App.css
+  
   return <div className="loader"></div>;
 }
 
